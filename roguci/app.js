5f0cf0293283612cced077be95f8a0a8cc0143bb
anggriyawan <anggri_nugraha@yahoo.com>
2021-08-28 09:43:31 +0700

    Update app.js

--- roguci/app.js
+++ roguci/app.js
@@ -11,8 +11,6 @@ const navSlide = () => {
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
-      } else {
-        link.style.animation = "navLinkFade 0.5s ease forwards ${index / 7}s";
       }
     });
     //Burger Animation
